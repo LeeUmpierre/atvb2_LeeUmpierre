097a9f8efdb5d9ef0714d5cb4550a87c256c1a20
LeeUmpierre <umpierre2507@hotmail.com>
2020-06-17 20:35:45 -0300

    17/06/20

--- app/controller/site.controller.js
+++ app/controller/site.controller.js
@@ -1,6 +1,13 @@
 (function(app){
-    app.controller('SiteController',function($scope){
+    app.controller('SiteController',function($scope, CategoriaService,FilmesService){
+        CategoriaService.listar().then(function(result){
+            $scope.filmes = [];
+            $scope.categorias = result.data;
 
+            FilmesService.listar().then(function(result2){
+                $scope.filmes = result2.data;
+            });
+        });
     });
     
 })(appFilmes);
